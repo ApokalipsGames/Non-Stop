d09f8267d365cab4685550a91b5f05547c3717a1
jkaz1968 <jkaz1968@gmail.com>
2016-05-27 07:51:37 -0500

    Level 1 Commit
    Level 1 and book script added.

---
+++ Nivelo 1.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1462232218737" ID="ID_225039293" MODIFIED="1462232867489" TEXT="Nivelo 1"/>
+</map>
